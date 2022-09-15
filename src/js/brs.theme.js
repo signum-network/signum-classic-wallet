34c6993d320bfcbfb3cfc821c8c100e0d9c12050
deleterium <rui.santana@protonmail.com>
2022-09-14 23:36:53 -0300

    Fix: dark-theme is now dark

--- src/js/brs.theme.js
+++ src/js/brs.theme.js
@@ -117,7 +117,7 @@ $(function() {
     })
     .append('<a href="javascript:void(0)" data-theme="theme-dark" style="display: block; box-shadow: 0 0 3px rgba(0,0,0,0.4)" class="clearfix full-opacity-hover">' +
       '<div><span style="display:block; width: 20%; float: left; height: 7px;" class="bg-dark-active"></span><span class="bg-dark" style="display:block; width: 80%; float: left; height: 7px;"></span></div>' +
-      '<div><span style="display:block; width: 20%; float: left; height: 20px; background: #222d32"></span><span style="display:block; width: 80%; float: left; height: 20px; background: #f4f5f7"></span></div>' +
+      '<div><span style="display:block; width: 20%; float: left; height: 20px; background: #222d32"></span><span style="display:block; width: 80%; float: left; height: 20px; background: #000000"></span></div>' +
       '</a>');
   $themesList.append($themeDark);
   $themeSettings.append($themesList);
